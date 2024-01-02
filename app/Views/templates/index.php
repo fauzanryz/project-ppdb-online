d46088c2c01e232af75320adb64238e8cf0e0cbf
Fauzan <fauzan@mhs.politala.ac.id>
2024-01-02 21:15:39 +0800

    Menambah icon

--- app/Views/templates/index.php
+++ app/Views/templates/index.php
@@ -6,6 +6,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>MTsN 4 Tanah Laut</title>
 
+  <!-- Icon -->
+  <link rel="shortcut icon" href="<?= base_url(); ?>/assets/logo.png">
+
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
